52e1fa07814012d1915494259a12a90cab1aa006
Kyle Farmer <kylefarmer85@gmail.com>
2020-09-01 23:13:49 +0000

    Done.

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -48,8 +48,8 @@ def checkout(cart, coupons)
    total += final_cart[i][:price] * final_cart[i][:count]
    i += 1
   end
-   if total > 100
-    return (total - (total * 0.1)) 
-   end
+  if total > 100
+    total -= (total * 0.10) 
+  end
   total
 end
